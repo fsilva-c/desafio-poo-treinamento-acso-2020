51e6a696068d4cd0f845bedec76ff0ec9d6a89e5
VitorM <victororeide2009@gmail.com>
2020-09-19 23:11:24 -0300

    Estruturacao dos diretorios e definicao do diagrama uml

---
+++ acso-city/sources/data.cpp
@@ -0,0 +1,67 @@
+#include "../headers/data.hpp"
+
+using namespace std;
+
+Data::Data(){}
+Data::Data(const Data &__data)
+{
+    dia = __data.dia;
+    mes = __data.mes;
+    ano = __data.ano;
+}
+Data::Data(unsigned long data)
+{
+    dia = data /1000000;
+    mes = (data /10000)%100;
+    ano = data %10000;
+}
+Data::Data(unsigned __dia, unsigned __mes, unsigned __ano)
+{
+    dia = __dia;
+    mes = __mes;
+    ano = __ano;
+}
+int Data::datacrp(Data data)
+{
+    int crp = 0;
+    crp = dia - data.getDia();
+    crp += mes *30 - data.getMes() *30;
+    crp += ano *365 - data.getAno() *365;
+    return crp;
+}
+void Data::printData()
+{
+    cout << dia 
+        << "/" << mes 
+        << "/" << ano << endl;
+}
+
+ostream& operator<<(ostream &os, const Data &data){
+    os << data.dia << '/' << data.mes << '/' << data.ano;
+    return os;
+}
+
+void Data::setDia(unsigned __dia)
+{
+    dia = __dia;
+}
+void Data::setMes(unsigned __mes)
+{
+    mes = __mes;
+}
+void Data::setAno(unsigned __ano)
+{
+    ano = __ano;
+}
+unsigned Data::getDia()
+{
+    return dia;
+}
+unsigned Data::getMes()
+{
+    return mes;
+}
+unsigned Data::getAno()
+{
+    return ano;
+}
---
+++ acso-city/tests/matriz.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <time.h> 
+
+using namespace std;
+
+#define L_MATRIZ 10
+#define C_MATRIZ 10
+
+enum TpPessoas{ hSaudavel = 1, mSaudavel, hInfectado, mInfectada, hVacinado, mVacinada };
+
+typedef struct cursor{
+    unsigned short linha, coluna;
+}CursorMatriz;
+
+void incializarMatriz(unsigned short matrix[][C_MATRIZ]);
+void exibirMatriz(unsigned short matrix[][C_MATRIZ]);
+void popularMatriz(unsigned short matrix[][C_MATRIZ], unsigned short qnt, unsigned short tpPessoa);
+unsigned short gerarPosMatriz();
+bool checkPosMatrizVazia(unsigned short matrix[][C_MATRIZ], unsigned short lMatrix, unsigned short cMatrix);
+unsigned short qntHabitantes(unsigned short matrix[][C_MATRIZ]);
+void passarUmAno(unsigned short matrix[][C_MATRIZ]);
+
+/* g++ -o matriz.o matriz.cpp && ./matriz.o */
+
+int main(){
+    srand(time(NULL));
+
+    unsigned short city[L_MATRIZ][C_MATRIZ];
+    
+    incializarMatriz(city);
+
+    popularMatriz(city, 1, mInfectada);
+    popularMatriz(city, 10, mSaudavel);
+    popularMatriz(city, 1, hInfectado);
+    popularMatriz(city, 10, hSaudavel);
+
+    exibirMatriz(city);
+    printf("Qnt Hab = %d\n", qntHabitantes(city));
+    
+    passarUmAno(city);
+    exibirMatriz(city);
+
+    return 0;
+}//END main
+
+
+void incializarMatriz(unsigned short matrix[][C_MATRIZ]){
+    /* Esta função inicializa a matriz com todos os índices zerados */
+
+    for(size_t i = 0; i < L_MATRIZ; i++)
+        for(size_t j = 0; j < C_MATRIZ; j++)
+            matrix[i][j] = 0;
+}//END function
+
+void exibirMatriz(unsigned short matrix[][C_MATRIZ]){
+    /* Esta função percorre a matriz exibindo os valores armazenados em cada índice */
+
+    for(size_t i = 0; i < L_MATRIZ; i++){
+        for(size_t j = 0; j < C_MATRIZ; j++)
+            cout << matrix[i][j] << " " ;
+
+        cout << endl;
+    }
+}//END function
+
+void popularMatriz(unsigned short matrix[][C_MATRIZ], unsigned short qnt, unsigned short tpPessoa){
+    /* Esta função popula a matriz a partir do tipo da pessoa e a quantidade que irá ser inserida na matrix */
+
+    unsigned short lMatriz, cMatriz;
+
+    for(size_t i = 0; i < qnt; ){
+        lMatriz = gerarPosMatriz(), cMatriz = gerarPosMatriz();
+
+        if(checkPosMatrizVazia(matrix, lMatriz, cMatriz)){
+            matrix[lMatriz][cMatriz] = tpPessoa;
+
+            i++;
+        }
+    }
+}//END function
+
+unsigned short gerarPosMatriz(){ 
+    /* Esta função gera um número aleatório entre 0 e 9 */
+    return rand() % 10;
+}//END function
+
+bool checkPosMatrizVazia(unsigned short matrix[][C_MATRIZ], unsigned short lMatrix, unsigned short cMatrix){
+    /* Esta função avalia se a matriz, no índice passado está por parâmetro, está com o valor zerado */
+
+    if(matrix[lMatrix][cMatrix] == 0) return true;
+    else return false;
+}//END function
+
+unsigned short qntHabitantes(unsigned short matrix[][C_MATRIZ]){
+    /* Esta função percorre a matriz e retorna a quantidade de habitantes na matriz */
+
+    unsigned short qntHab = 0;
+
+    for(size_t i = 0; i < L_MATRIZ; i++)
+        for(size_t j = 0; j < C_MATRIZ; j++)
+            if(matrix[i][j] != 0)
+                qntHab++;
+
+    return qntHab;
+}//END function
+
+void passarUmAno(unsigned short matrix[][C_MATRIZ]){
+    /* IMCOMPLETA */
+    
+    CursorMatriz aux;
+
+    //for(size_t i = 0; i < qntHabitantes(matrix); ){
+        
+        for(size_t j = 0; j < L_MATRIZ; j++){
+            for(size_t k = 0; k < C_MATRIZ; k++){
+                if(matrix[j][k] != 0)
+                    cout << j << " " << k << endl;
+            }
+        }
+    //}
+}//END function
